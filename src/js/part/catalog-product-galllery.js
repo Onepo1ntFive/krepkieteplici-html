b8e4436c01fb93a7def20e7f4c1900f95dc33863
SergeySolovev <name.diesel@gmail.com>
2021-04-18 22:29:58 +0300

    детальная начало верстки

---
+++ src/js/part/catalog-product-galllery.js
@@ -0,0 +1,47 @@
+(function () {
+
+    const tinySliderNav = tns({
+        container: '.js-tiny-nav',
+        nav: false,
+        controls: false,
+        axis: 'vertical',
+        items: 5,
+        slideBy: 1,
+        mouseDrag: true,
+        loop: true,
+        center: true,
+        onInit: (info) => {
+            // console.log(info);
+            info.slideItems[info.index].classList.add('tns-item-current');
+        }
+    });
+    const tinySliderMain = tns({
+        container: '.js-tiny-main',
+        nav: false,
+        controls: false,
+        axis: 'vertical',
+        slideBy: 1,
+        mouseDrag: false,
+        loop: true,
+        touch: false,
+        center: true,
+        onInit: (info) => {
+            console.log(info);
+            info.slideItems[info.index].classList.add('tns-item-current');
+        }
+    });
+
+    tinySliderNav.events.on('indexChanged', (info) => {
+        console.log(tinySliderNav.getInfo(), tinySliderMain.getInfo())
+    });
+
+    tinySliderNav.events.on("transitionEnd", function (info) {
+        info.slideItems[info.indexCached].classList.remove('tns-item-current');
+        info.slideItems[info.index].classList.add('tns-item-current');
+    });
+    tinySliderMain.events.on("transitionEnd", function (info) {
+        info.slideItems[info.indexCached].classList.remove('tns-item-current');
+        info.slideItems[info.index].classList.add('tns-item-current');
+    });
+
+})();
