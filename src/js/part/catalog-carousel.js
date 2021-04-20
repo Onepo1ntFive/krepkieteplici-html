1a47bf0d59ad72660120050c4f46a4cf9c6bc088
Sergey Solovyev <html@laserwar.com>
2021-04-20 18:08:08 +0300

    Карусель каталога

---
+++ src/js/part/catalog-carousel.js
@@ -0,0 +1,26 @@
+(function () {
+    const swiperCatalogCarousel = new Swiper('.swiper-container--catalog', {
+        loop: true,
+        watchSlidesVisibility: true,
+        slidesPerView: 4,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            500: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            1200: {
+                slidesPerView: 4,
+            }
+        }
+    });
+})();
